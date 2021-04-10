6b3e9058179dfbd6e8886198f57dca64dc68238a
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-04-11 00:45:35 +0300

    create models

--- loop/models.py
+++ loop/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class NeighborHood(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    occupants = models.PositiveIntegerField()
+    
+    # admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
+
+
+class profileUser(models.Model):
+    prof_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(blank=True)
+    neighborHood = models.ForeignKey(NeighborHood, on_delete=models.CASCADE, blank=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+
+
+class Bussiness(models.Model):
+    biz_name = models.CharField(max_length=100, blank=True)
+    neighborhood = models.ForeignKey(NeighborHood, on_delete=models.CASCADE, blank=True)
+    profile = models.ForeignKey(profileUser, on_delete=models.CASCADE, blank=True)
+    biz_email = models.EmailField(blank=True)
