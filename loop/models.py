ee35cd74fff8b5afce850539446550065ddd7629
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-04-11 08:57:22 +0300

    include business model foreign key to profile user model

--- loop/models.py
+++ loop/models.py
@@ -10,18 +10,24 @@ class NeighborHood(models.Model):
     
     # admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
 
+    
 class profileUser(models.Model):
     prof_name = models.CharField(max_length=100, blank=True)
-    profile_pic = CloudinaryField('image')
+    profile_pic = CloudinaryField('image', blank=True)
     email = models.EmailField(blank=True)
-    neighborHood = models.ForeignKey(NeighborHood, on_delete=models.CASCADE, blank=True)
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    neighborHood = models.ForeignKey(NeighborHood, on_delete=models.CASCADE, blank=True, null=True)
+    business = models.ForeignKey('Bussiness', on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
+    def save_user(self):
+        self.save()
 
 class Bussiness(models.Model):
     biz_name = models.CharField(max_length=100, blank=True)
-    neighborhood = models.ForeignKey(NeighborHood, on_delete=models.CASCADE, blank=True)
-    profile = models.ForeignKey(profileUser, on_delete=models.CASCADE, blank=True)
+    neighborhood = models.ForeignKey(NeighborHood, on_delete=models.CASCADE, blank=True, null=True)
+    profile = models.ForeignKey(profileUser, on_delete=models.CASCADE, blank=True, null=True)
     biz_email = models.EmailField(blank=True)
