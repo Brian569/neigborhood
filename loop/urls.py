7d7ed8b4f69e48e8ec5925dbc0f9cc5b65bf0c0f
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-04-11 08:54:58 +0300

    update profile view path

--- loop/urls.py
+++ loop/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
-from .views import (home, )
+from django.urls import path, re_path
+from .views import (home, logout_view,
+    profile, updataProfile
+     )
+
 urlpatterns = [
     path('', home, name = 'home'),
+    path('logout/', logout_view, name ='logouts'),
+    re_path(r'profile/(\d+)',profile,name = 'profile'),
+    path('update/', updataProfile, name = 'update'),
 ]
