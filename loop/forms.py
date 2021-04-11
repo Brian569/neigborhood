ede99338529cfd9c15c0fdfebd7cedd82a2e7693
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-04-11 16:00:08 +0300

    add details in profile form

--- loop/forms.py
+++ loop/forms.py
@@ -4,5 +4,18 @@ from .models import *
 class ProfileupdateForm(forms.ModelForm):
     class Meta:
         model = profileUser
-        fields = ['profile_name', 'profile_pic', 'email']
-        exclude = ['user', 'neighborHood']
+        fields = ['profile_name', 'profile_pic', 'email', 'location', 'bio']
+        exclude = ['user']
+
+
+class BussinessForm(forms.ModelForm):
+    class Meta:
+        model = Bussiness
+        fields = ['business_name', 'business_email', 'neighborhood']
+        exclude = ['owner']
+
+class NeighborHoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighborHood
+        fields = ['name', 'location']
+        exclude = ['resider']
