19f5a351a4610328cad74bd5d88722c2d3bedc46
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-04-11 17:20:10 +0300

    add posts, create post, single hood

--- loop/urls.py
+++ loop/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, re_path
 from .views import (home, logout_view,
     profile, updataProfile, business,
-    my_business, create_neigborhood
+    my_business, create_neigborhood, posts,
+    create_post, single_hood
      )
 
 urlpatterns = [
@@ -11,5 +12,8 @@ urlpatterns = [
     path('update/', updataProfile, name = 'update'),
     path('business/', business, name = 'business'),
     re_path(r'my_business/(\d+)', my_business, name = 'my_business'),
-    path('neigbor/', create_neigborhood, name = 'neigbor')
+    path('neigbor/', create_neigborhood, name = 'neigbor'),
+    path('posts/', posts, name = 'posts'),
+    path('create_post/', create_post, name = 'create_post'),    
+    re_path(r'single_hood/(\d+)', single_hood, name = 'single_hood'),
 ]
