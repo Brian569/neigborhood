66ae779c1ab85afd9381d2aed30e88885421c043
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-04-11 17:19:13 +0300

    posts modele class

--- loop/models.py
+++ loop/models.py
@@ -46,5 +46,10 @@ class profileUser(models.Model):
     def save_user(self):
         self.save()
 
+class Posts(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    content = models.TextField(blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
-    
+    def __str__(self):
+        return self.title
