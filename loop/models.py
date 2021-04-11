96500b1817f0ab21e19351b2563612d52019059f
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-04-11 16:00:47 +0300

    add variables to profile class

--- loop/models.py
+++ loop/models.py
@@ -2,32 +2,49 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-
 class NeighborHood(models.Model):
     name = models.CharField(max_length=100, blank=True)
     location = models.CharField(max_length=100, blank=True)
-    occupants = models.PositiveIntegerField()
+    resider = models.ForeignKey(User, on_delete=models.CASCADE, null= True)
     
-    # admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
-
     def __str__(self):
         return self.name
 
-    
+    def delete_neib(self):
+        self.delete()
+
+    def save_neib(self):
+        self.name
+
+
+class Bussiness(models.Model):
+    business_name = models.CharField(max_length=100, blank=True)
+    neighborhood = models.ForeignKey(NeighborHood, on_delete=models.CASCADE, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    business_email = models.EmailField(blank=True)
+
+    def save_biz(self):
+        self.save()
+
+    def __str__(self):
+        return self.business_name
+
+    def get_biz(self):
+        business = Bussiness.objects.all()
+        return business
+
 class profileUser(models.Model):
     profile_name = models.CharField(max_length=100, blank=True)
     profile_pic = CloudinaryField('image', blank=True)
     email = models.EmailField(blank=True)
+    bio = models.CharField(max_length=1000, blank=True)
 
-    neighborHood = models.ForeignKey(NeighborHood, on_delete=models.CASCADE, blank=True, null=True)
-    business = models.ForeignKey('Bussiness', on_delete=models.CASCADE, null=True)
+    location = models.ForeignKey(NeighborHood, on_delete=models.CASCADE, null=True)
+    business = models.ForeignKey(Bussiness, on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
     def save_user(self):
         self.save()
 
-class Bussiness(models.Model):
-    biz_name = models.CharField(max_length=100, blank=True)
-    neighborhood = models.ForeignKey(NeighborHood, on_delete=models.CASCADE, blank=True, null=True)
-    profile = models.ForeignKey(profileUser, on_delete=models.CASCADE, blank=True, null=True)
-    biz_email = models.EmailField(blank=True)
+
+    
