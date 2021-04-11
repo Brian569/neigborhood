9348e28e81286e855a87b953826e2aed5abf7795
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-04-11 16:01:33 +0300

    create business and neighborhood paths

--- loop/urls.py
+++ loop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
 from .views import (home, logout_view,
-    profile, updataProfile
+    profile, updataProfile, business,
+    my_business, create_neigborhood
      )
 
 urlpatterns = [
@@ -8,4 +9,7 @@ urlpatterns = [
     path('logout/', logout_view, name ='logouts'),
     re_path(r'profile/(\d+)',profile,name = 'profile'),
     path('update/', updataProfile, name = 'update'),
+    path('business/', business, name = 'business'),
+    re_path(r'my_business/(\d+)', my_business, name = 'my_business'),
+    path('neigbor/', create_neigborhood, name = 'neigbor')
 ]
